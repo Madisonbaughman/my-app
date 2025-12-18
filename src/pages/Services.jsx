2a92c006853696e89a20321e8b6bba01916ae467
sabidul07 <166614504+sabidul07@users.noreply.github.com>
2025-12-18 14:06:43 +0530

    service section add links

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -53,7 +53,6 @@ function Services() {
             <ImgSkeleton className="w-full max-w-xl aspect-[4/3]" />
           )}
         </div>
-
         <div className="w-full max-w-[564px] mx-auto md:mx-0">
           <p className="text-para mb-8">
             I provide concierge occupational therapy and personal training
@@ -75,84 +74,96 @@ function Services() {
       <div className="mt-10 lg:mt-20 mb-10 lg:mb-[100px]">
         <div className="custom-container !py-0 flex gap-6 lg:flex-nowrap flex-wrap justify-center lg:justify-start">
           {/* Card 1 */}
-          <div className="bg-white w-full max-w-[364px] rounded-xl overflow-clip">
-            <div className="w-full flex justify-center">
-              {card1Ready ? (
-                <img
-                  className=""
-                  src={photo2}
-                  alt="Personal Training & Wellness"
-                />
-              ) : (
-                <ImgSkeleton className="w-full max-w-md aspect-[4/3] m-2" />
-              )}
+          <div className="bg-white w-full max-w-[364px] rounded-xl overflow-clip flex flex-col justify-between pb-4">
+            <div>
+              <div className="w-full flex justify-center">
+                {card1Ready ? (
+                  <img
+                    className=""
+                    src={photo2}
+                    alt="Personal Training & Wellness"
+                  />
+                ) : (
+                  <ImgSkeleton className="w-full max-w-md aspect-[4/3] m-2" />
+                )}
+              </div>
+              <div className="p-4">
+                <h6 className="mb-2 text-2xl font-['Cormorant_Infant'] font-bold">
+                  Personal Training &amp; Wellness
+                </h6>
+                <p className="font-['Cormorant_Infant'] font-normal leading-[150%]">
+                  Concierge training (in-home or on-site in NYC) Concierge
+                  training (in-home or on-site in NYC) Personal Training and
+                  Wellness Strength training Pilates-informed movement
+                  Functional fitness programs Life stage–specific training
+                  (postpartum recovery, osteoporosis, healthy aging)
+                </p>
+              </div>
             </div>
-            <div className="p-4">
-              <h6 className="mb-2 text-2xl font-['Cormorant_Infant'] font-bold">
-                Personal Training &amp; Wellness
-              </h6>
-              <p className="font-['Cormorant_Infant'] font-normal leading-[150%]">
-                Concierge training (in-home or on-site in NYC) Concierge
-                training (in-home or on-site in NYC) Personal Training and
-                Wellness Strength training Pilates-informed movement Functional
-                fitness programs Life stage–specific training (postpartum
-                recovery, osteoporosis, healthy aging)
-              </p>
-            </div>
+            <Link to="/Contact" className="btn ml-4 !mt-4">
+              CONTACT US
+            </Link>
           </div>
 
           {/* Card 2 */}
-          <div className="bg-white w-full max-w-[364px] rounded-xl overflow-clip">
-            <div className="">
-              {card2Ready ? (
-                <img
-                  className=""
-                  src={photo4}
-                  alt="Rehabilitation & Hand Therapy"
-                />
-              ) : (
-                <ImgSkeleton className="w-full max-w-md aspect-[4/3] m-2" />
-              )}
+          <div className="bg-white w-full max-w-[364px] rounded-xl overflow-clip flex flex-col justify-between pb-4">
+            <div>
+              <div>
+                {card2Ready ? (
+                  <img src={photo4} alt="Rehabilitation & Hand Therapy" />
+                ) : (
+                  <ImgSkeleton className="w-full max-w-md aspect-[4/3] m-2" />
+                )}
+              </div>
+              <div className="p-4">
+                <h6 className="mb-2 text-2xl font-['Cormorant_Infant'] font-bold">
+                  Rehabilitation &amp; Hand Therapy
+                </h6>
+                <p className="font-['Cormorant_Infant'] font-normal leading-[150%]">
+                  Orthopedic and neurological rehab Certified Hand Therapy (CHT)
+                  for hand, wrist, elbow, and shoulder Custom splint fabrication
+                  Recovery after injury, surgery, or repetitive strain
+                  Arthritis, tendon/ligament injuries, nerve compressions,
+                  rotator cuff, fractures, CRPS
+                </p>
+              </div>
             </div>
-            <div className="p-4">
-              <h6 className="mb-2 text-2xl font-['Cormorant_Infant'] font-bold">
-                Rehabilitation &amp; Hand Therapy
-              </h6>
-              <p className="font-['Cormorant_Infant'] font-normal leading-[150%]">
-                Orthopedic and neurological rehab Certified Hand Therapy (CHT)
-                for hand, wrist, elbow, and shoulder Custom splint fabrication
-                Recovery after injury, surgery, or repetitive strain Arthritis,
-                tendon/ligament injuries, nerve compressions, rotator cuff,
-                fractures, CRPS
-              </p>
-            </div>
+            <Link to="/Contact" className="btn ml-4 !mt-4">
+              CONTACT US
+            </Link>
           </div>
 
           {/* Card 3 */}
-          <div className="bg-white w-full max-w-[364px] rounded-xl overflow-clip">
-            <div className="w-full flex justify-center">
-              {card3Ready ? (
-                <img className="" src={photo5} alt="Specialized Programs" />
-              ) : (
-                <ImgSkeleton className="w-full max-w-md aspect-[4/3] m-2" />
-              )}
+          <div className="bg-white w-full max-w-[364px] rounded-xl overflow-clip flex flex-col justify-between pb-4">
+            <div>
+              <div>
+                {card3Ready ? (
+                  <img className="" src={photo5} alt="Specialized Programs" />
+                ) : (
+                  <ImgSkeleton className="w-full max-w-md aspect-[4/3] m-2" />
+                )}
+              </div>
+              <div className="p-4">
+                <h6 className="mb-2 text-2xl font-['Cormorant_Infant'] font-bold">
+                  Specialized Remote Programs
+                </h6>
+                <p className="font-['Cormorant_Infant'] font-normal leading-[150%]">
+                  Virtual Strength Training and Programming Prenatal &
+                  postpartum exercise support (without internal exams)
+                  Rehab-informed training for osteoporosis, osteopenia, and
+                  osteoarthritis Preventive care programs for long-term joint
+                  and muscle health I also work holistically with other trusted
+                  practitioners to ensure clients receive comprehensive support.
+                  When needed, I connect clients with my network of recommended
+                  pelvic floor specialists, physical therapists, dietitians, and
+                  physicians — collaborating across disciplines to help you feel
+                  your best.
+                </p>
+              </div>
             </div>
-            <div className="p-4">
-              <h6 className="mb-2 text-2xl font-['Cormorant_Infant'] font-bold">
-                Specialized Remote Programs
-              </h6>
-              <p className="font-['Cormorant_Infant'] font-normal leading-[150%]">
-                Virtual Strength Training and Programming Prenatal & postpartum
-                exercise support (without internal exams) Rehab-informed
-                training for osteoporosis, osteopenia, and osteoarthritis
-                Preventive care programs for long-term joint and muscle health I
-                also work holistically with other trusted practitioners to
-                ensure clients receive comprehensive support. When needed, I
-                connect clients with my network of recommended pelvic floor
-                specialists, physical therapists, dietitians, and physicians —
-                collaborating across disciplines to help you feel your best.
-              </p>
-            </div>
+            <Link to="/Contact" className="btn ml-4 !mt-4">
+              CONTACT US
+            </Link>
           </div>
         </div>
       </div>
