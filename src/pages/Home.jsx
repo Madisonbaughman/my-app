4968e1c418818580459540629710322ecda92203
sabidul07 <166614504+sabidul07@users.noreply.github.com>
2025-12-21 00:03:42 +0530

    Update heading and paragraph text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,13 +15,10 @@ function Home() {
       <section className="flex justify-between gap-10 flex-col lg:flex-row items-center">
         <div className="w-full max-w-[520px]">
           <h1 className="text-[26px] sm:text-3xl lg:text-[46px] font-bold font-['Cormorant_Infant'] mb-4">
-            Move better. Feel stronger. Live well in your body. Strength
-            training and rehabilitation in NYC
+            Move better. Feel stronger
           </h1>
           <p className="text-lg font-bold text-black/70 font-['Cormorant_Infant'] mb-8">
-            MBODY Better is a holistic wellness space focused on restoring
-            balance, improving movement, and helping you reconnect with your
-            body through mindful care and expert guidance.
+            Strength training and rehabilitation in NYC
           </p>
           <Link to="/Contact" className="btn">
             CONTACT US
